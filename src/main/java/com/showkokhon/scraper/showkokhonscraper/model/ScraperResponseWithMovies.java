87478a977c1efdaad48c6a8dc080b0023995235a
Shawon Ashraf <shawon13@live.com>
2019-06-14 22:43:13 +0600

    separate defn for class

--- src/main/java/com/showkokhon/scraper/showkokhonscraper/model/ScraperResponseWithMovies.java
+++ src/main/java/com/showkokhon/scraper/showkokhonscraper/model/ScraperResponseWithMovies.java
@@ -1,12 +1,18 @@
 package com.showkokhon.scraper.showkokhonscraper.model;
 
 import java.util.ArrayList;
+import java.util.Date;
 
-public class ScraperResponseWithMovies extends BasicScraperResponse {
+public class ScraperResponseWithMovies  {
+    public int STATUS_CODE;
+    public String MSG;
+    public String SENT_AT;
     public ArrayList<Movie> movies;
 
-    public ScraperResponseWithMovies(int STATUS_CODE, String DATA, ArrayList<Movie> movies) {
-        super(STATUS_CODE, DATA);
+    public ScraperResponseWithMovies(int STATUS_CODE, String MSG, ArrayList<Movie> movies) {
+        this.STATUS_CODE = STATUS_CODE;
+        this.MSG = MSG;
         this.movies = movies;
+        SENT_AT = new Date().toString();
     }
 }
