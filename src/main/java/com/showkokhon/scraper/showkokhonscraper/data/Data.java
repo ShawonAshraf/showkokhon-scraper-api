a1e684c372cf3a7ac4c6ba5e8494cd956bf8853c
Shawon Ashraf <shawon13@live.com>
2019-06-14 23:32:33 +0600

    class to keep constants

---
+++ src/main/java/com/showkokhon/scraper/showkokhonscraper/data/Data.java
@@ -0,0 +1,6 @@
+package com.showkokhon.scraper.showkokhonscraper.data;
+
+public class Data {
+    public static final String bcity = "Bashundhara Shopping Mall, Panthapath";
+    public static final String ss = "Shimanto Shambhar, Dhanmondi 2";
+}
