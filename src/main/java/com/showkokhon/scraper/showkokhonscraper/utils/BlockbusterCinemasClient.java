a032ff62f65927ecc1d0bacf70288c4a55f07091
Shawon Ashraf <shawon13@live.com>
2019-06-18 20:31:18 +0600

    bulk fetch client

--- src/main/java/com/showkokhon/scraper/showkokhonscraper/utils/BlockbusterCinemasClient.java
+++ src/main/java/com/showkokhon/scraper/showkokhonscraper/utils/BlockbusterCinemasClient.java
@@ -2,13 +2,23 @@ package com.showkokhon.scraper.showkokhonscraper.utils;
 
 import com.showkokhon.scraper.showkokhonscraper.model.BasicScraperResponse;
 import kong.unirest.Unirest;
+import org.jsoup.Jsoup;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class BlockbusterCinemasClient {
     // generic error message
     private final String WEBSITE_ERROR = "Couldn't Fetch Data Due To WebSite Error";
     private final String url = "https://blockbusterbd.com/schedule.php";
 
-    public BasicScraperResponse fetch(String date) {
+    private final ArrayList<String> dates;
+
+    public BlockbusterCinemasClient() {
+        dates = new ArrayList<>();
+    }
+
+    private BasicScraperResponse _fetch(String date) {
         var requestString = String.format("%s?request=%s", url, date);
         var response = Unirest.get(requestString).asString();
 
@@ -19,4 +29,34 @@ public class BlockbusterCinemasClient {
                         response.getBody()) : new BasicScraperResponse(
                 response.getStatus(), WEBSITE_ERROR, null);
     }
+
+    public BasicScraperResponse fetch() {
+        prepareDateList();
+        var data = new StringBuilder();
+
+        try {
+            dates.forEach(date -> {
+                // make request
+                var response = _fetch(date);
+                data.append(response.DATA);
+            });
+
+            return new BasicScraperResponse(200, "OK", data.toString());
+        } catch (Exception e) {
+            return new BasicScraperResponse(
+                    500, WEBSITE_ERROR, null);
+        }
+    }
+
+    // fetch the next 7 days dates
+    private void prepareDateList() {
+        var today = LocalDate.now();
+        dates.add(today.toString());
+
+        for (long i = 1; i < 7; i++) {
+            var date = today.plusDays(i);
+            dates.add(date.toString());
+        }
+    }
+
 }
