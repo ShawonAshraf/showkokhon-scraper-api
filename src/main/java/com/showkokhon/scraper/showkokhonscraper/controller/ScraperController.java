cdee7c2b21849b43000434622b96c0db80803f33
ShawonAshraf <shawon13@live.com>
2021-10-29 04:33:51 +0200

    revived

--- src/main/java/com/showkokhon/scraper/showkokhonscraper/controller/ScraperController.java
+++ src/main/java/com/showkokhon/scraper/showkokhonscraper/controller/ScraperController.java
@@ -48,8 +48,7 @@ public class ScraperController {
 
     @RequestMapping(value = "/scraper/v1/schedule/all", method = RequestMethod.GET)
     public ScraperResponseWithMovies getAllSchedule() {
-        // var movies = Fetcher.getAllMovies();
-        var movies = Fetcher.getAllStarCineplexMovies();
+        var movies = Fetcher.getAllMovies();
         return new ScraperResponseWithMovies(200, movies);
     }
 
